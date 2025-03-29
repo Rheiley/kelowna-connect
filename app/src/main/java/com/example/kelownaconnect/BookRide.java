c73f89e3ac6b7da965ed16cff206d88c2137f3c8
Rheiley <rheileyzoom@gmail.com>
2025-03-29 10:13:52 -0700

    Add views in MainActivity.java

--- app/src/main/java/com/example/kelownaconnect/BookRide.java
+++ app/src/main/java/com/example/kelownaconnect/BookRide.java
@@ -1,7 +1,6 @@
 package com.example.kelownaconnect;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,26 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class BookRide extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_book_ride);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        // Views
-        Button bookRideButton = findViewById(R.id.bookRideButton);
-        Button offerRideButton = findViewById(R.id.offerRideButton);
-        Button rewardsButton = findViewById(R.id.rewardsButton);
-        Button withdrawButton = findViewById(R.id.withdrawButton);
-        Button activeRidesButton = findViewById(R.id.activeRidesButton);
-        Button settingsButton = findViewById(R.id.settingsButton);
-
     }
 }
